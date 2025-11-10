dc4c947d44ec63a83c467b276862cde5dba804c5
hesam khani <kebciv12@gmail.com>
2025-11-10 22:23:50 +0330

    my task

---
+++ script.js
@@ -0,0 +1,21 @@
+for (let i =1; i<= 20; i++) {
+    if(i % 3 ===0 && i % 5 ===0){
+        console.log(i, "cointrap")
+    }else if (i % 3===0){
+        console.log(i, "coin");
+    }else if (i % 5===0){
+        console.log(i , "trap");
+    }
+}
+const coin = 6
+const trap =4
+const cointrap = 1
+const all = 11
+console.log("coin=" ,coin);
+console.log("trap=" ,trap);
+console.log("cointrap=" ,cointrap);
+console.log("all=" ,all);
+
+
+
+
